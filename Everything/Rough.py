46f8df4a0db475827c73d029b71c469f07921a2a
akhil <ashah01581@gmail.com>
2019-12-01 00:59:29 -0500

    fds

--- Everything/Rough.py
+++ Everything/Rough.py
@@ -1,31 +1,25 @@
+#https://www.youtube.com/watch?v=ZDa-Z5JzLYM
 #print("***to create class")
 class House:
 	pass
 
 #print("instance variable")
 class Employee:
-	def __init__(self,first,last,pay):
-		self.first=first
-		self.last=last
-		self.pay=pay
-		self.email= first + '.' + last + "@company.com"
+	def __init__(self,first2,last2,pay2):
+		self.first1=first2
+		self.last1=last2
+		self.pay1=pay2
+		self.email1= first2 + '.' + last2 + "@company.com"
 
 	def fullname(self):
-		return self.first
+		return self.first1
 
 emp1= Employee('kartik','patel',5000)
 emp2=Employee('Hari','gotipatti',10000)
 
-print(emp1.email)
-print(emp2.email)
-print(emp2.fullname)
-
-12:00 
-
-
-
-
-
+print(emp1.email1)
+print(emp2.email1)
+print(emp2.fullname())
 
 #print("class variable")
 class MyClass:

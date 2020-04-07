e70a1e3448cb5bf807a8e5dcb021a75815b14499
akhil <ashah01581@gmail.com>
2020-04-07 13:32:12 +0530

    adasfas

--- Everything/Rough.py
+++ Everything/Rough.py
@@ -1,15 +1,1 @@
-#prime numbers
-n=10
-m=list(range(2,n+1))
 
-def prime(n):
-	if n>0:
-		if n//2 == 1:
-			print(str(n) +' is prime')	
-		elif n//3 == 1:
-			print(str(n) + ' is prime')
-		elif n//5 == 1:
-			print(str(n)+ ' is prime')
-
-for x in m:
-	prime(x)
